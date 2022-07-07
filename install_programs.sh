a54c5282437e37c38db3e61a1613b3759282ab3f
Max Spahn <m.spahn@tudelft.nl>
2022-07-07 09:46:29 +0200

    Adds fresh install.

---
+++ install_programs.sh
@@ -0,0 +1,12 @@
+#sudo apt install git
+sudo apt install zsh
+#wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
+#sudo apt install ./nvim-linux64.deb
+sudo apt install npm
+curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install v16.15.0
+nvm use v16.15.0
+sudo npm install -g pyright
+sudo apt install python3-pip
+pip3 install neovim
+
